a17e7a5724e743a7b2c36ded62612e0474b103f0
nikola3794 <nikolapopovic3794@yahoo.com>
2016-12-03 19:19:58 +0100

    Updated z3-serial-port.py to process incoming bytes

--- Python/3. Reading a datastream on a serial port/z3-serial-port.py
+++ Python/3. Reading a datastream on a serial port/z3-serial-port.py
@@ -1,36 +1,76 @@
-'''
-fh=open('file.txt','wb')
-s=bytearray()
-s.append(2)
-s.append(72)
-s.append(10)
-s.append(80)
-s=s+b'lalala'
-fh.write(s)
 
-fh.close()
+def emptyMsg():
+    message = {
+            "stx" : False,
+            "cmd" : -1,
+            "len":-1,
+            "data" : bytearray(),
+            "crc" : -1
+        }
 
-fh=open('file.txt','rb')
-print(fh.read().decode())
-'''
-import time
-import struct
+    return message
+
+def process(data,message):
+    for b in data:
+
+        if b == 2 and message["stx"] == False:
+            message["stx"] = True
+            continue
+
+        elif message["stx"] == True and message["cmd"] == -1:
+            message["cmd"] = b
+            message["crc"] = 2 ^ b
+            continue
+
+        elif message["cmd"] != -1 and message["len"] == -1:
+            message["len"] = b
+            message["crc"] ^= b
+            continue
+
+        elif message["len"] != -1 and len(message["data"]) < message["len"]:
+            message["data"].append(b)
+            message["crc"] ^= b
+            continue
+
+        elif len(message["data"]) == message["len"]:
+            if message["crc"] == b:
+                msg=''
+                for el in message["data"]:
+                    msg += str(el) + ' '
+
+                print("Command", message["cmd"], "found -> printing command data: ")
+                print(msg,"(Message length:",message["len"],")")
+
+            else:
+                print("There has been an error while trying to recieve a message.")
+
+            message["stx"] = False
+            message["cmd"] = -1
+            message["len"] = -1
+            message["data"] = bytearray()
+            message["crc"] = -1
+
 
 def main():
-    fh=open('file.txt','rb')
 
-    while True:
-        a=fh.read(1)
+    try:
+        fh=open('message.bin','rb')
 
-        if (a == b''):
-            break
+    except IOError as e:
+        print(e)
 
-        print(a)
-        num=struct.unpack('B', a)[0]
-        print(num)
-        time.sleep(1)
+    else:
+        data = fh.read(9)
 
+        message = emptyMsg()
 
+        while data != b'':
+            process(data, message)
+
+            data = fh.read(9)
+
+        print("Terminated with message",message)
+        fh.close()
 
 
 
