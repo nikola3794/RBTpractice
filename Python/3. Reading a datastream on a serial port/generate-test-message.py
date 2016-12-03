54b633621c8c328db957fc7fba05f8be41ddcd1b
nikola3794 <nikolapopovic3794@yahoo.com>
2016-12-03 20:51:13 +0100

    Changed the comments

--- Python/3. Reading a datastream on a serial port/generate-test-message.py
+++ Python/3. Reading a datastream on a serial port/generate-test-message.py
@@ -8,45 +8,45 @@ Outputs: msg
 Generates a message and outputs it.
 '''
 def generateMessage():
-    msg = bytearray() # Create an empty message
-    msg = msg + b'2578eguovu034hbh8w4vu1cj923' # Add random bytes to the message
+    msg = bytearray()  # Create an empty message
+    msg = msg + b'2578eguovu034hbh8w4vu1cj923'  # Add random bytes to the message
 
     # Add 4 valid commands surrounded with random bytes to the message
     for j in range(4):
 
         # Start of a command
         msg.append(2)  # STX byte
-        msg.append(random.randint(0,127))  # CMD
-        len = 2 ** j
-        msg.append(len)  # DataLen
+        msg.append(random.randint(0, 127))  # CMD
+        l = 2 ** j
+        msg.append(l)  # DataLen
 
-        for el in range(len):
-            msg.append(random.randint(0,255))  # D[i]
+        for el in range(l):
+            msg.append(random.randint(0, 255))  # D[i]
 
         # CRC byte is computed as a XOR of all the bytes in the command
         crc = 2
-        for i in msg[-(len + 2):]:
+        for i in msg[-(l + 2):]:
             crc ^= i
         msg.append(crc)
         # End of a command
 
-        msg = msg + b'asdsfu69byhjv5ynvjtv4ny04h3nveh9v3huu395379tyvhb9jas' # Add random bytes to the message
+        msg = msg + b'asdsfu69byhjv5ynvjtv4ny04h3nveh9v3huu395379tyvhb9jas'  # Add random bytes to the message
 
     # Add 3 invalid commands surrounded with random bytes to the message
     for j in range(3):
 
         # Start of a command
         msg.append(2)  # STX byte
-        msg.append(random.randint(0,127))  # CMD
-        len = 2 ** (j + 1)
-        msg.append(len)  # DataLen
+        msg.append(random.randint(0, 127))  # CMD
+        l = 2 ** (j + 1)
+        msg.append(l)  # DataLen
 
-        for el in range(len-1):
-            msg.append(random.randint(0,255))  # D[i]
+        for el in range(l-1):
+            msg.append(random.randint(0, 255))  # D[i]
 
         # The command lacks 1 additional data byte and 1 crc byte
 
-        msg = msg + b'6789654345678987654rfgbnjhgfdfghjkkjgfd' # Add random bytes to the message
+        msg = msg + b'6789654345678987654rfgbnjhgfdfghjkkjgfd'  # Add random bytes to the message
 
     # Add 1 valid command surrounded with random bytes to the message
     for j in range(1):
@@ -54,28 +54,28 @@ def generateMessage():
         # Start of a command
         msg.append(2)  # STX byte
         msg.append(random.randint(0, 127))  # CMD
-        len = 5
-        msg.append(len)  # DataLen
+        l = 5
+        msg.append(l)  # DataLen
 
-        for el in range(len):
+        for el in range(l):
             msg.append(random.randint(0, 255))  # D[i]
 
         # CRC byte is computed as a XOR of all the bytes in the message
         crc = 2
-        for i in msg[-(len + 2):]:
+        for i in msg[-(l + 2):]:
             crc ^= i
         msg.append(crc)
         # End of a command
 
-        msg = msg + b'lkjhgtr65789okljhgt678ijhgt678ijnbvgftr56789' # Add random bytes to the message
+        msg = msg + b'lkjhgtr65789okljhgt678ijhgt678ijnbvgftr56789'  # Add random bytes to the message
 
     # Add 1 half-complete command surrounded with random bytes to the message
     # Start of a command
     msg.append(2)  # STX byte
     msg.append(random.randint(0, 127))  # CMD
-    len = 4
-    msg.append(len) # DataLen
-    msg.append(80) # D[1]
+    l = 4
+    msg.append(l)  # DataLen
+    msg.append(80)  # D[1]
 
     # The command lacks 3 additional data byte and 1 crc byte
 
@@ -83,11 +83,11 @@ def generateMessage():
 
 
 def main():
-    fileName='message.bin' # File path of the binary file, where the message will be stored
+    fileName = 'message.bin'  # File path of the binary file, where the message will be stored
 
     msg = generateMessage()  # Generate the message to be stored
 
-    #Trys to write the message to the binary file
+    # Try's to write the message to the binary file
     try:
         fh = open(fileName, 'wb')
     except IOError:
