8aa588fe06b0b4cfc68e8b66a126fbb990d902e2
nikola3794 <nikolapopovic3794@yahoo.com>
2016-12-03 19:19:03 +0100

    Made changes to generate-test-message.py to add 3 valid messages, then 2 invalid, then 1 valid , and finally 1 half complete

--- Python/3. Reading a datastream on a serial port/generate-test-message.py
+++ Python/3. Reading a datastream on a serial port/generate-test-message.py
@@ -1,36 +1,77 @@
+import random
+
 '''
-Generates a message which is to be stored in a binary file and later processed.
+Generates a message and stores it in a binary file.
 '''
 def generateMessage():
     x = bytearray()
+    x = x + b'2578eguovu034hbh8w4vu1cj923'
 
-    for j in range(3):
-
-        x = x + b'ajsdfhjkdaskafjsk'
+    # 4 Correct messages
+    for j in range(4):
 
         #Adds a command to the message
         x.append(2)  # STX byte
-        x.append(12)  # CMD = 12
-        len = 3
-        x.append(len)  # DataLen = 3
-        x.append(80)  # D[0]
-        x.append(79)  # D[1]
-        x.append(80)  # D[2]
+        x.append(random.randint(0,127))  # CMD
+        len = 2 ** j
+        x.append(len)  # DataLen
+        for el in range(len):
+            x.append(random.randint(0,255))  # D[i]
 
         # CRC byte is computed as a XOR of all the bytes in the message
         crc = 2
         for i in x[-(len + 2):]:
-            crc = crc ^ i
+            crc ^= i
         x.append(crc)
 
-        x = x + b'ashjdfkjkadsfjas'
+        x = x + b'asdsfu69byhjv5ynvjtv4ny04h3nveh9v3huu395379tyvhb9jas'
+
+    # 3 Incorrect messages
+    for j in range(3):
+
+        #Adds a command to the message
+        x.append(2)  # STX byte
+        x.append(random.randint(0,127))  # CMD
+        len = 2 ** (j + 1)
+        x.append(len)  # DataLen
+        for el in range(len-1):
+            x.append(random.randint(0,255))  # D[i]
+
+        x = x + b'6789654345678987654rfgbnjhgfdfghjkkjgfd'
+
+    # 1 Correct messages
+    for j in range(1):
+
+        # Adds a command to the message
+        x.append(2)  # STX byte
+        x.append(random.randint(0, 127))  # CMD
+        len = 2 ** (j+2) #Len = 2
+        x.append(len)  # DataLen
+        for el in range(len):
+            x.append(random.randint(0, 255))  # D[i]
+
+        # CRC byte is computed as a XOR of all the bytes in the message
+        crc = 2
+        for i in x[-(len + 2):]:
+            crc ^= i
+        x.append(crc)
+
+        x = x + b'lkjhgtr65789okljhgt678ijhgt678ijnbvgftr56789'
+
+    # One half complete message
+    x.append(2)  # STX byte
+    x.append(random.randint(0, 127))  # CMD
+    len = 2 ** (j + 2)
+    x.append(len) # DataLen
+    x.append(80)
+
 
     return x
 
 
 def main():
     x=generateMessage()
-    fileName='message.txt'
+    fileName='message.bin'
 
     #Trys to write the message to a binary file
     try:
