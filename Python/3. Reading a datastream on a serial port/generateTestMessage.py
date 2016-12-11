884609f4fdc11f8e9e6ebbcabfeda81ff33f3674
nikola3794 <nikolapopovic3794@yahoo.com>
2016-12-11 20:36:59 +0100

    Renamed the script and made minor changes

--- Python/3. Reading a datastream on a serial port/generateTestMessage.py
+++ Python/3. Reading a datastream on a serial port/generateTestMessage.py
@@ -1,21 +1,27 @@
+'''
+generate-test-message.py
+'''
+
 import random
 
+STX = 2
+
 '''
-Function: generateMessage()
+Function: generateMessage
 Inputs:
 Outputs: msg
 
-Generates a message and outputs it.
+Generates a message - msg and outputs it.
 '''
 def generateMessage():
+
     msg = bytearray()  # Create an empty message
-    # msg = msg + b'2578eguovu034hbh8w4vu1cj923'  # Add random bytes to the message
 
     # Add 4 valid commands surrounded with random bytes to the message
     for j in range(4):
 
         # Start of a command
-        msg.append(2)  # STX byte
+        msg.append(STX)  # STX byte
         msg.append(random.randint(0, 127))  # CMD
         l = 2 ** j
         msg.append(l)  # DataLen
@@ -24,35 +30,34 @@ def generateMessage():
             msg.append(random.randint(0, 255))  # D[i]
 
         # CRC byte is computed as a XOR of all the bytes in the command
-        crc = 2
+        crc = STX
         for i in msg[-(l + 2):]:
             crc ^= i
         msg.append(crc)
         # End of a command
 
-        msg = msg + b'asdsfu69byhjv5ynvjtv4ny04h3nveh9v3huu395379tyvhb9jas'  # Add random bytes to the message
+        msg += b'9js'  # Add random bytes to the message
 
     # Add 3 invalid commands surrounded with random bytes to the message
     for j in range(3):
 
         # Start of a command
-        msg.append(2)  # STX byte
+        msg.append(STX)  # STX byte
         msg.append(random.randint(0, 127))  # CMD
         l = 2 ** (j + 1)
         msg.append(l)  # DataLen
 
         for el in range(l-1):
             msg.append(random.randint(0, 255))  # D[i]
-
         # The command lacks 1 additional data byte and 1 crc byte
 
-        msg = msg + b'6789654345678987654rfgbnjhgfdfghjkkjgfd'  # Add random bytes to the message
+        msg += b'67gfd'  # Add random bytes to the message
 
     # Add 1 valid command surrounded with random bytes to the message
     for j in range(1):
 
         # Start of a command
-        msg.append(2)  # STX byte
+        msg.append(STX)  # STX byte
         msg.append(random.randint(0, 127))  # CMD
         l = 5
         msg.append(l)  # DataLen
@@ -61,40 +66,59 @@ def generateMessage():
             msg.append(random.randint(0, 255))  # D[i]
 
         # CRC byte is computed as a XOR of all the bytes in the message
-        crc = 2
+        crc = STX
         for i in msg[-(l + 2):]:
             crc ^= i
         msg.append(crc)
         # End of a command
 
-        msg = msg + b'lkjhgtr65789okljhgt678ijhgt678ijnbvgftr56789'  # Add random bytes to the message
+        msg += b'lkp'  # Add random bytes to the message
 
     # Add 1 half-complete command surrounded with random bytes to the message
     # Start of a command
-    msg.append(2)  # STX byte
+    msg.append(STX)  # STX byte
     msg.append(random.randint(0, 127))  # CMD
     l = 4
     msg.append(l)  # DataLen
     msg.append(80)  # D[1]
-
     # The command lacks 3 additional data byte and 1 crc byte
 
     return msg
 
 
-def main():
-    fileName = 'message.bin'  # File path of the binary file, where the message will be stored
 
-    msg = generateMessage()  # Generate the message to be stored
+'''
+Function: messageToBinaryFile
+Inputs: fileName
+Outputs:
+
+Calls generateMessage() to generate a message.
+Writes the message to a binary file - fileName.
+'''
+def messageToBinaryFile(fileName):
+
+    msg = generateMessage()  # Generate the message
 
     # Try's to write the message to the binary file
     try:
         fh = open(fileName, 'wb')
+
     except IOError:
         print("Couldn't open ", fileName, " in wb mode.")
+
     else:
         fh.write(msg)
         fh.close()
+
+
+
+def main():
+
+    fileName = 'message.bin'  # File path of the binary file, where the message will be stored
+
+    messageToBinaryFile(fileName)  # Generate a message and write it to a binary file
+
+
 
 if __name__ == "__main__":
     main()
